f69680f066122b4b1c840fa0751f6bbec961f4b8
Luca Padula <lucamp9843@gmail.com>
2021-05-29 20:21:36 -0400

    Add home and pagenotfound routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,20 @@ import { FormsModule } from "@angular/forms"
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { SidebarComponent } from "./sidebar/sidebar.component"
-import { ArtistsComponent } from "./artists/artists.component";
-import { LoginComponent } from './login/login.component'
+import { ArtistsComponent } from "./artists/artists.component"
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 @NgModule({
-   declarations: [AppComponent, SidebarComponent, ArtistsComponent, LoginComponent],
+   declarations: [
+      AppComponent,
+      SidebarComponent,
+      ArtistsComponent,
+      LoginComponent,
+      HomeComponent,
+      PageNotFoundComponent,
+   ],
    imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
    providers: [],
    bootstrap: [AppComponent],
