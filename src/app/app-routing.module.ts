624abc29eb26fc7b46b4bd984775b5a12475a01b
Luca Padula <lucamp9843@gmail.com>
2021-06-01 13:06:13 -0400

    Add authentication route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { ArtistsComponent } from "./artists/artists.component"
 import { LoginComponent } from "./login/login.component"
 import { HomeComponent } from "./home/home.component"
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component"
+import { AuthGuard } from "./shared/guards/auth.guard"
 
 const routes: Routes = [
    { path: "home", component: HomeComponent },
-   { path: "artists", component: ArtistsComponent },
+   { path: "artists", component: ArtistsComponent, canActivate: [AuthGuard] },
    { path: "login", component: LoginComponent },
    { path: "", redirectTo: "/home", pathMatch: "full" },
    { path: "**", component: PageNotFoundComponent },
---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core"
+import {
+   ActivatedRouteSnapshot,
+   CanActivate,
+   RouterStateSnapshot,
+   Router,
+} from "@angular/router"
+import { AuthService } from "../services/auth.service"
+
+@Injectable({
+   providedIn: "root",
+})
+export class AuthGuard implements CanActivate {
+   constructor(private authService: AuthService, private router: Router) {}
+
+   canActivate(
+      route: ActivatedRouteSnapshot,
+      state: RouterStateSnapshot
+   ): boolean {
+      if (!this.authService.userIsAuthenticated()) {
+         this.router.navigate(["/login"])
+         return false
+      }
+      return true
+   }
+}
