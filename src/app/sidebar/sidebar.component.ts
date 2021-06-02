e195c3952a999d59f280e7f2393efc71aeb330e6
Luca Padula <lucamp9843@gmail.com>
2021-06-02 00:03:51 -0400

    Add conditional login/logout in sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core"
+import { Router, Event, NavigationStart } from "@angular/router"
+import { Subject } from "rxjs"
+import { takeUntil } from "rxjs/operators"
 import { AuthService } from "../shared/services/auth.service"
 
 @Component({
@@ -7,11 +10,28 @@ import { AuthService } from "../shared/services/auth.service"
    styleUrls: ["./sidebar.component.css"],
 })
 export class SidebarComponent implements OnInit {
-   private token: any
+   private ngUnsubscribe = new Subject()
+   public token: any
 
-   constructor(private authService: AuthService) {
-      this.token = authService.getDecodedToken()
+   constructor(private authService: AuthService, private router: Router) {}
+
+   ngOnInit(): void {
+      this.router.events
+         .pipe(takeUntil(this.ngUnsubscribe))
+         .subscribe((event: Event) => {
+            if (event instanceof NavigationStart) {
+               this.token = this.authService.getDecodedToken()
+            }
+         })
    }
 
-   ngOnInit(): void {}
+   logout(): void {
+      this.authService.logout()
+      this.router.navigate(["login"])
+   }
+
+   ngOnDestroy(): void {
+      this.ngUnsubscribe.next()
+      this.ngUnsubscribe.complete()
+   }
 }
