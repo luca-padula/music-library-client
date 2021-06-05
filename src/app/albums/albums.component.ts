6f1913a639cf0d81992bb7814aec3dec9f975844
Luca Padula <lucamp9843@gmail.com>
2021-06-05 18:03:40 -0400

    Add boilerplate for album sorting in component

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,7 +1,21 @@
 import { Component, OnInit } from "@angular/core"
-import { Observable } from "rxjs"
+import { BehaviorSubject, combineLatest, Observable } from "rxjs"
+import { map } from "rxjs/operators"
 import { Album } from "../shared/models/album"
 import { AlbumService } from "../shared/services/album.service"
+
+interface SortPredicate {
+   field: string
+   descending: boolean
+}
+
+const sortPredicates = new Map<string, SortPredicate>([
+   ["none", { field: "", descending: false }],
+   ["albumNameAsc", { field: "name", descending: false }],
+   ["albumNameDesc", { field: "name", descending: true }],
+   ["artistNameAsc", { field: "artistName", descending: false }],
+   ["artistNameDesc", { field: "artistName", descending: true }],
+])
 
 @Component({
    selector: "app-albums",
@@ -10,10 +24,40 @@ import { AlbumService } from "../shared/services/album.service"
 })
 export class AlbumsComponent implements OnInit {
    albums$: Observable<Album[]>
+   sortPredicate$ = new BehaviorSubject<SortPredicate>({
+      field: "",
+      descending: false,
+   })
 
    constructor(private albumService: AlbumService) {
-      this.albums$ = this.albumService.getAllAlbums()
+      this.albums$ = combineLatest([
+         this.albumService.getAllAlbums(),
+         this.sortPredicate$,
+      ]).pipe(
+         map(([albums, predicate]) => {
+            const sortField = predicate.field as keyof Album
+            return albums.sort((album, nextAlbum) => {
+               if (album[sortField] < nextAlbum[sortField]) {
+                  return -1
+               }
+               if (album[sortField] > nextAlbum[sortField]) {
+                  return 1
+               }
+               return 0
+            })
+            /* return albums.sort((album, nextAlbum) =>
+               album["artistName"].localeCompare(nextAlbum["artistName"])
+            ) */
+         })
+      )
    }
+
+   /* generateCompareFunction(predicate: SortPredicate): () => number {
+    const sortField = predicate.field as keyof Album
+    return (album1: Album, album2: Album) => {
+
+    }
+   } */
 
    ngOnInit(): void {}
 }
