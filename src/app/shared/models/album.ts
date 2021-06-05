0807e938d6063079cde1ad3bbc2cfce6b7e3ee9e
Luca Padula <lucamp9843@gmail.com>
2021-06-05 15:32:45 -0400

    Add album model and service.

---
+++ src/app/shared/models/album.ts
@@ -0,0 +1,11 @@
+export interface Album {
+   _id: string
+   name: string
+   releaseDate: Date
+   albumLength: string
+   artist: string
+   artistName: string
+   createdAt: string
+   updatedAt: string
+   __v: number
+}
