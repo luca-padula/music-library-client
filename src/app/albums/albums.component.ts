1f55c4e3a0933143ef1db11b11355151934e3d40
Luca Padula <lucamp9843@gmail.com>
2021-06-08 00:37:05 -0400

    Add searching with rxjs in albums component

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit } from "@angular/core"
-import { BehaviorSubject, combineLatest, Observable } from "rxjs"
-import { map } from "rxjs/operators"
+import { AfterViewInit, Component, OnInit } from "@angular/core"
+import { BehaviorSubject, combineLatest, fromEvent, Observable } from "rxjs"
+import { FromEventTarget } from "rxjs/internal/observable/fromEvent"
+import {
+   debounceTime,
+   distinctUntilChanged,
+   map,
+   pluck,
+   startWith,
+   tap,
+} from "rxjs/operators"
 import { Album } from "../shared/models/album"
 import { AlbumService } from "../shared/services/album.service"
 
@@ -22,27 +30,18 @@ const sortPredicates = new Map<string, SortPredicate>([
    templateUrl: "./albums.component.html",
    styleUrls: ["./albums.component.css"],
 })
-export class AlbumsComponent implements OnInit {
-   albums$: Observable<Album[]>
+export class AlbumsComponent implements OnInit, AfterViewInit {
+   allAlbums$ = this.albumService.getAllAlbums()
+   searchInput$ = new Observable<string>()
    sortPredicate$ = new BehaviorSubject<SortPredicate>({
       field: "",
       descending: false,
    })
+   albums$ = new Observable<Album[]>()
 
-   constructor(private albumService: AlbumService) {
-      this.albums$ = combineLatest([
-         this.albumService.getAllAlbums(),
-         this.sortPredicate$,
-      ]).pipe(
-         map(([albums, predicate]) => {
-            const albumCompareFunction =
-               this.generateAlbumCompareFunction(predicate)
-            return albums.sort(albumCompareFunction)
-         })
-      )
-   }
+   constructor(private albumService: AlbumService) {}
 
-   generateAlbumCompareFunction(
+   buildAlbumCompareFunction(
       predicate: SortPredicate
    ): (album1: Album, album2: Album) => number {
       const sortField = predicate.field as keyof Album
@@ -58,4 +57,37 @@ export class AlbumsComponent implements OnInit {
    }
 
    ngOnInit(): void {}
+
+   ngAfterViewInit(): void {
+      const searchInput = document.querySelector(
+         "#searchInput"
+      ) as FromEventTarget<Event>
+
+      this.searchInput$ = fromEvent(searchInput, "keyup").pipe(
+         map((event) => event.target as HTMLInputElement),
+         pluck("value"),
+         debounceTime(400),
+         distinctUntilChanged(),
+         startWith("")
+      )
+
+      this.albums$ = combineLatest([
+         this.allAlbums$,
+         this.searchInput$,
+         this.sortPredicate$,
+      ]).pipe(
+         map(([albums, filter, predicate]) => {
+            const compareFunction = this.buildAlbumCompareFunction(predicate)
+            return albums
+               .filter(
+                  (album) =>
+                     album.name.toLowerCase().includes(filter.toLowerCase()) ||
+                     album.artistName
+                        .toLowerCase()
+                        .includes(filter.toLowerCase())
+               )
+               .sort(compareFunction)
+         })
+      )
+   }
 }
