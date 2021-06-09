070bdb6f7ccda0aa018ce2146a29271ce65dc1c7
Luca Padula <lucamp9843@gmail.com>
2021-06-09 15:57:33 -0400

    Add dynamic sorting with bug that needs fixing
    Add select element to choose sort order in albums component
    Sorting has bug with change detection

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -15,14 +15,6 @@ interface SortPredicate {
    descending: boolean
 }
 
-const sortPredicates = new Map<string, SortPredicate>([
-   ["none", { field: "", descending: false }],
-   ["albumNameAsc", { field: "name", descending: false }],
-   ["albumNameDesc", { field: "name", descending: true }],
-   ["artistNameAsc", { field: "artistName", descending: false }],
-   ["artistNameDesc", { field: "artistName", descending: true }],
-])
-
 @Component({
    selector: "app-albums",
    templateUrl: "./albums.component.html",
@@ -36,6 +28,14 @@ export class AlbumsComponent implements OnInit {
    */
    @ViewChild("searchInput", { static: true })
    searchInputEl!: ElementRef<HTMLInputElement>
+
+   sortPredicates = new Map<string, SortPredicate>([
+      ["none", { field: "", descending: false }],
+      ["albumNameAsc", { field: "name", descending: false }],
+      ["albumNameDesc", { field: "name", descending: true }],
+      ["artistNameAsc", { field: "artistName", descending: false }],
+      ["artistNameDesc", { field: "artistName", descending: true }],
+   ])
 
    getAllAlbums$ = this.albumService.getAllAlbums()
    searchInput$ = new Observable<string>()
@@ -83,14 +83,23 @@ export class AlbumsComponent implements OnInit {
       predicate: SortPredicate
    ): (album1: Album, album2: Album) => number {
       const sortField = predicate.field as keyof Album
+      const descending = predicate.descending
       return (album1: Album, album2: Album) => {
          if (album1[sortField] < album2[sortField]) {
-            return -1
+            return descending ? 1 : -1
          }
          if (album1[sortField] > album2[sortField]) {
-            return 1
+            return descending ? -1 : 1
          }
          return 0
+      }
+   }
+
+   handleSortOptionChange(event: Event): void {
+      const target = event.target as HTMLInputElement
+      const newSortOption = this.sortPredicates.get(target.value)
+      if (newSortOption) {
+         this.sortPredicate$.next(newSortOption)
       }
    }
 }
