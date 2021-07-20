cf8cdcbf2c43a811bc339b82c9e8322698a13b57
Luca Padula <lucamp9843@gmail.com>
2021-07-19 23:10:04 -0400

    Add form to edit playlist

--- src/app/playlists/edit-playlist/edit-playlist.component.ts
+++ src/app/playlists/edit-playlist/edit-playlist.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
+import { ActivatedRoute } from "@angular/router"
+import { emptyPlaylist, Playlist } from "../playlist"
 
 @Component({
-  selector: 'app-edit-playlist',
-  templateUrl: './edit-playlist.component.html',
-  styleUrls: ['./edit-playlist.component.css']
+   selector: "app-edit-playlist",
+   templateUrl: "./edit-playlist.component.html",
+   styleUrls: ["./edit-playlist.component.css"],
 })
 export class EditPlaylistComponent implements OnInit {
+   playlist = emptyPlaylist
 
-  constructor() { }
+   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
-  }
-
+   ngOnInit(): void {
+      this.playlist = this.route.snapshot.data.playlist as Playlist
+   }
 }
