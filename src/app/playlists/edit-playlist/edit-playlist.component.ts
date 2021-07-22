57c01771ec6fcf529872b136f9c26ad77a30a3b7
Luca Padula <lucamp9843@gmail.com>
2021-07-22 15:37:59 -0400

    Add update playlist function to edit playlist

--- src/app/playlists/edit-playlist/edit-playlist.component.ts
+++ src/app/playlists/edit-playlist/edit-playlist.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from "@angular/core"
+import { NgForm } from "@angular/forms"
 import { ActivatedRoute } from "@angular/router"
+import { Subject } from "rxjs"
+import { take } from "rxjs/operators"
+import { ApiError } from "src/app/shared/models/api-error"
 import { emptyPlaylist, Playlist } from "../playlist"
+import { PlaylistService } from "../playlist.service"
 
 @Component({
    selector: "app-edit-playlist",
@@ -9,10 +14,28 @@ import { emptyPlaylist, Playlist } from "../playlist"
 })
 export class EditPlaylistComponent implements OnInit {
    playlist = emptyPlaylist
+   updateSuccessNotifier = new Subject<string>()
+   updateErrorNotifier = new Subject<ApiError>()
 
-   constructor(private route: ActivatedRoute) {}
+   constructor(
+      private route: ActivatedRoute,
+      private playlistService: PlaylistService
+   ) {}
 
    ngOnInit(): void {
       this.playlist = this.route.snapshot.data.playlist as Playlist
+   }
+
+   onSubmit(form: NgForm): void {
+      const { name, isPrivate } = this.playlist
+      const fieldsToUpdate: Partial<Playlist> = { name, isPrivate }
+      this.playlistService
+         .updatePlaylist(this.playlist._id, fieldsToUpdate)
+         .pipe(take(1))
+         .subscribe(
+            (success) =>
+               this.updateSuccessNotifier.next(`Successfully updated playlist`),
+            (err: ApiError) => this.updateErrorNotifier.next(err)
+         )
    }
 }
